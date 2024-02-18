7645e389d2eaa7b12b33809cb32ca23c6bf895b2
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-02-18 22:43:44 +0100

    Test Window

--- src/main/java/com/emfisgraphics/bagsuit/bagcore/TestApplication.java
+++ src/main/java/com/emfisgraphics/bagsuit/bagcore/TestApplication.java
@@ -1,9 +1,13 @@
 package com.emfisgraphics.bagsuit.bagcore;
 
 import com.emfisgraphics.bagsuit.bagcore.application.BagApplication;
+import com.emfisgraphics.bagsuit.bagcore.utils.Window;
 
 public class TestApplication {
+    // Software Enter Point
     public static void main(String[] args) {
-
+        //Singelton
+        Window window = Window.get();
+        window.run();
     }
 }
