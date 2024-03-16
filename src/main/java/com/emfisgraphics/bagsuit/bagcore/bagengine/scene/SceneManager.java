6aa588f02cd5a4f35d41342096ef0c344903411a
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-03-16 23:06:35 +0100

    SceneManager init

---
+++ src/main/java/com/emfisgraphics/bagsuit/bagcore/bagengine/scene/SceneManager.java
@@ -0,0 +1,30 @@
+package com.emfisgraphics.bagsuit.bagcore.bagengine.scene;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+----------------------------------------------------------------------------
+                        Bag Core - SceneManager
+                     Copyright (C) Márton Bán 2024
+
+
+----------------------------------------------------------------------------
+*/
+
+
+public class SceneManager {
+    private static Map<Integer, Scene> scenes = new HashMap<>();
+
+    void addScene(String path) {
+
+    }
+
+    void deleteScene(int ID) {
+
+    }
+
+    void reloadScene(int ID) {
+
+    }
+}
